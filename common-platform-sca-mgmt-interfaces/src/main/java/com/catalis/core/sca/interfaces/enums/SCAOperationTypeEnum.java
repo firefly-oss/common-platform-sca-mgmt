e76f43f91a6edea34a4174f3a57d461ebbe41741
Carlos Sánchez <casc84ab@gmail.com>
2025-05-27 18:33:46 +0200

    Add ONBOARDING type to SCAOperationTypeEnum
    Extend the SCAOperationTypeEnum with the ONBOARDING type to represent scenarios requiring SCA during the onboarding process. This improves clarity and support for additional use cases within the enum.

--- common-platform-sca-mgmt-interfaces/src/main/java/com/catalis/core/sca/interfaces/enums/SCAOperationTypeEnum.java
+++ common-platform-sca-mgmt-interfaces/src/main/java/com/catalis/core/sca/interfaces/enums/SCAOperationTypeEnum.java
@@ -5,5 +5,6 @@ public enum SCAOperationTypeEnum {
     PASSWORD_CHANGE, // e.g., user changing password
     LOGIN,           // e.g., user login (step-up SCA)
     LOAN_REQUEST,    // e.g., user applying for a loan
+    ONBOARDING,      // e.g., onboarding process requiring SCA
     OTHER            // fallback for unspecified types
 }
